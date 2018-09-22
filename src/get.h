89c1e51902779c642f4d71686acb11cb03090083
Matthew Ferderber <matthewferderber@gmail.com>
2018-09-22 15:59:02 -0400

    restructure code

--- src/get.h
+++ src/get.h
@@ -6,6 +6,15 @@
 #ifndef A1_40010150_GET_H
 #define A1_40010150_GET_H
 
+#include "request.h"
+
+struct get_request {
+	struct http_url url;
+	struct http_header http_headers*;
+	struct http_query_parameters query_parameters*;
+};
+
+void http_get(int sockfd, struct get_request req, struct http_response* res);
 
 
 #endif //A1_40010150_GET_H
---
+++ src/types.h
@@ -0,0 +1,9 @@
+//
+// Created by Matthew Ferderber
+// COMP445 Assignment #1
+//
+
+#ifndef A1_40010150_TYPES_H
+#define A1_40010150_TYPES_H
+
+#endif //A1_40010150_TYPES_H
